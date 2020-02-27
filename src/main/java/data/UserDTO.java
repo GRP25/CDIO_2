88668fe8e93f3301dda84699fcf35b6c6d85ca0c
Martin <mail@mama.sh>
2020-02-27 18:01:43 +0100

    meger with userDTO

---
+++ src/main/java/data/UserDTO.java
@@ -0,0 +1,60 @@
+package data;
+
+import java.util.List;
+
+
+public class UserDTO{
+    String name;
+    String password;
+    List<String> roles;
+    int id;
+    int cpr;
+
+    public UserDTO(String name, String password, List<String> roles, int id, int cpr) {
+        this.name = name;
+        this.password = password;
+        this.roles = roles;
+        this.id = id;
+        this.cpr = cpr;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCpr() {
+        return cpr;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCpr(int cpr) {
+        this.cpr = cpr;
+    }
+}
