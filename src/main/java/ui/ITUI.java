d3e43a650f851bfc1785da353156ea22f0e5e7d9
Daniel <s143861@student.dtu.dk>
2020-02-27 16:59:31 +0100

    Started work on TUI implementation

--- src/main/java/ui/ITUI.java
+++ src/main/java/ui/ITUI.java
@@ -1,5 +1,11 @@
 package ui;
 
-public interface ITUI{
-
+public interface ITUI {
+    void chooseDataBase();
+    void showMenu(int i);
+    void createUser();
+    void showUserList();
+    void deleteUser();
+    void updateUser();
+    void exit();
 }
