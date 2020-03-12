1819b41327edf6e708ebcea3a2a749e38c2faf84
Daniel <s143861@student.dtu.dk>
2020-03-12 16:07:22 +0100

    Expanded TUI, and setup realtionship with func layer WIP

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,9 @@
+import data.text.DALException;
 import ui.ITUI;
 import ui.UserTUI;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws DALException {
         ITUI tui = new UserTUI();
         tui.chooseDataBase();
     }
--- src/main/java/ui/ITUI.java
+++ src/main/java/ui/ITUI.java
@@ -5,7 +5,7 @@ import data.text.DALException;
 public interface ITUI {
     void chooseDataBase() throws DALException;
     void showMenu(int i) throws DALException;
-    void createUser();
+    void createUser() throws DALException;
     void showUserList() throws DALException;
     void deleteUser() throws DALException;
     void updateUser();
