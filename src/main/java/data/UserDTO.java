257a67ee63471b667d153e59d0b491ecfc6d785f
Mohamad-Ashmar <s176492@student.dtu.dk>
2020-03-13 16:14:06 +0100

    need  serialVersionUID in userDTO

--- src/main/java/data/UserDTO.java
+++ src/main/java/data/UserDTO.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class UserDTO implements Serializable {
+    private static final long serialVersionUID = 150643518387140262L;
     private String name;
     private String initials;
     private String password;
