cad4cb6d738cf91ccc4cdd6fae19bcc7e553c985
Oliver Comprés Poulsen <oli@live.dk>
2020-04-28 17:22:44 +0200

    addet ";"

--- src/main/java/data/IUserDAO.java
+++ src/main/java/data/IUserDAO.java
@@ -2,15 +2,13 @@ package data;
 
 //import data.memory.UserDAO;
 
-import data.text.DALException;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public interface IUserDAO{
-    UserDTO getUser(int userID)
-    void updateUser(UserDTO user)
-    void createUser(UserDTO user)
-    void deleteUser(int userID)
-    ArrayList<UserDTO> getUserList()
+    UserDTO getUser(int userID);
+    void updateUser(UserDTO user);
+    void createUser(UserDTO user);
+    void deleteUser(int userID);
+    ArrayList<UserDTO> getUserList();
 }
