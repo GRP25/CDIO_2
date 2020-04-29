99be0e5655086026d94def7509f3b61ed456743e
Martin <mail@mama.sh>
2020-04-29 13:31:07 +0200

    ny branch

--- src/main/java/data/sql/Initiation.java
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS user (
-   user_id integer PRIMARY KEY AUTOINCREMENT,
-   user_name text NOT NULL,
-   user_init text NOT NULL,
-   user_cpr integer NOT NULL UNIQUE,
-   user_password text NOT NULL,
-   user_groups text NOT NULL
-);
