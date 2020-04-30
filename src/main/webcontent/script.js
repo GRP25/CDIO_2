4f791030fe64aa164538571264db936effb1c04d
Daniel <s143861@student.dtu.dk>
2020-04-30 19:50:18 +0200

    Javascript functions added without indhold

--- src/main/webcontent/script.js
+++ src/main/webcontent/script.js
@@ -1,4 +1,59 @@
 
-    function formHandler(form) {
-        console.log('you typed ' + form);
+$(document).ready(() => {
+    loadDatabase();
+});
+
+
+    function formHandler() {
+        event.preventDefault();
+
+        let password = $('#pass').val();
+        let username = $('#bNavn').val();
+
+        if(password == 'test' && username == 'test') {
+            $('#loginScreen').hide();
+            $('#sh').show();
+            $('#sr').show();
+        } else {
+            alert("Wrong username and/or password");
+
+        }
+    }
+
+
+
+
+
+
+function createUser() {
+    $('#resultview').hide();
+
+    //call function
+    $.ajax('java/sql/main', post)
+
+    //load data into view displayet to user
+    $('.createUser').load(data)
+
+    //show container
+    $('createUser').show();
+}
+
+function updateUser() {
+
+}
+
+function getUser() {
+
+}
+
+function listUser() {
+
+}
+
+function deleteUser() {
+
+}
+
+function loadDatabase() {
+
 }
